b9b4d2b85f88d309e0aa4023521eeb3dacf4677a
rohitshar <rohitshar@zeta.tech>
2024-08-04 14:01:15 +0530

    TRC-18615: Universal link update

--- script.js
+++ script.js
@@ -1,13 +1,20 @@
-import copy from copy-to-clipboard
+// import { copy } from './node_modules/copy-to-clipboard/index.js';
 
-const copyContent = async () => {
-    try {
-      await navigator.clipboard.writeText("Copy success 2");
-      console.log('Content copied to clipboard');
-    } catch (err) {
-      console.error('Failed to copy: ', err);
-    }
-}
+setTimeout(async () => {
+    await navigator.clipboard.writeText("Your text").then(function () {
+       console.log("Copy to clipboard successfully.");
+    }, function () {
+       console.log("Copy to clipboard unsuccessfully.");
+    });
+});
+
+// const copyContent = async () => {
+//     try {
+//       await navigator.clipboard.writeText("Copy success 2");
+//       console.log('Content copied to clipboard');
+//     } catch (err) {
+//       console.error('Failed to copy: ', err);
+//     }
+// }
 console.log('Content to clipboard');
-copyContent();
-copy('rohit sharma is indian captain....');
+// copyContent();// copy('rohit sharma is indian captain....');
