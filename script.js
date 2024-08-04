545fe971108f3b641937786549b572526c0fc413
rohitshar <rohitshar@zeta.tech>
2024-08-04 14:16:23 +0530

    TRC-18615: Universal link update

--- script.js
+++ script.js
@@ -1,12 +1,12 @@
 // import { copy } from './node_modules/copy-to-clipboard/index.js';
 
-setTimeout(async () => {
-    navigator.clipboard.writeText("Your text").then(function () {
-       console.log("Copy to clipboard successfully.");
-    }, function () {
-       console.log("Copy to clipboard unsuccessfully.");
-    });
-});
+// setTimeout(async () => {
+//     navigator.clipboard.writeText("Your text").then(function () {
+//        console.log("Copy to clipboard successfully.");
+//     }, function () {
+//        console.log("Copy to clipboard unsuccessfully.");
+//     });
+// });
 
 // const copyContent = async () => {
 //     try {
@@ -16,5 +16,29 @@ setTimeout(async () => {
 //       console.error('Failed to copy: ', err);
 //     }
 // }
+
+const clipboardItem = new ClipboardItem({
+    'text/plain': someAsyncMethod().then((result) => {
+
+    /**
+     * We have to return an empty string to the clipboard if something bad happens, otherwise the
+     * return type for the ClipBoardItem is incorrect.
+     */
+    if (!result) {
+        return new Promise(async (resolve) => {
+            resolve(new Blob[``]())
+        })
+    }
+
+    const copyText = `some string`
+        return new Promise(async (resolve) => {
+            resolve(new Blob([copyText]))
+        })
+    }),
+})
+// Now, we can write to the clipboard in Safari
+navigator.clipboard.write([clipboardItem])
+
 console.log('Content to clipboard');
-// copyContent();// copy('rohit sharma is indian captain....');
+// copyContent();
+// copy('rohit sharma is indian captain....');
